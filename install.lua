f19fa56efdbd8334af8aa5c2749586ecf9ee4b2a
eternalled <eternal.led@gmail.com>
2016-09-15 19:40:17 -0400

    fixed issue with install

--- install.lua
+++ install.lua
@@ -1,36 +1,36 @@
 -- drmon installation script
-
+ 
 local libURL = "https://raw.githubusercontent.com/eternalled/DraconicReactorMonitor/master/lib/f.lua"
 local startupURL = "https://raw.githubusercontent.com/eternalled/DraconicReactorMonitor/master/startup.lua"
-local programURL = "https://raw.githubusercontent.com/eternalled/DraconicReactorMonitor/master/drmon.lua"
-
+local drmpURL = "https://raw.githubusercontent.com/eternalled/DraconicReactorMonitor/master/drmon.lua"
+ 
 local lib, startup, drmp
 local libFile, startupFile, drmpFile
-
+ 
 local file1, file2, file3
-
+ 
 fs.makeDir("lib")
-
+ 
 lib = http.get(libURL)
 libFile = lib.readAll()
-
+ 
 --Get library files
-local file1 = fs.open("lib/f", "w")
+file1 = fs.open("lib/f", "w")
 file1.write(libFile)
 file1.close()
-
+ 
 --Startup Script
 startup = http.get(startupURL)
 startupFile = startup.readAll()
-
+ 
 file2 = fs.open("startup", "w")
 file2.write(startupFile)
 file2.close()
-
+ 
 --Draconic Reactor Monitor Program
 drmp = http.get(drmpURL)
 drmpFile = drmp.readAll()
-
+ 
 file3 = fs.open("drmp", "w")
-file2.write(drmpFile)
-file2.close()
+file3.write(drmpFile)
+file3.close()
